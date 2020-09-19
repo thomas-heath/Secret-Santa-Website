4ebf7a743f83090f88ffc6daee6f576a2dbf5089
Thomas Heath <thomasheathtgh@gmail.com>
2020-09-19 16:39:31 +0100

    First commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = new express;
+const port = process.env.PORT || 9090;
+
+app.get("/", function(req, res){res.sendfile("./site.html")});
+app.get("/backend", function(req, res){res.sendfile("./secretsanta.js")});
+app.listen(port);

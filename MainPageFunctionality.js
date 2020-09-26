f6c734b047c27878e20b5e78dcd0fab478e53ef8
Thomas Heath <thomasheathtgh@gmail.com>
2020-09-26 17:45:15 +0100

    Tried to create a function that would modify the error element rather than creating a new one, but it didn't work.

--- MainPageFunctionality.js
+++ MainPageFunctionality.js
@@ -1,14 +1,36 @@
-function ensureCorrectInput(inputText) {
+//function displayInputError(errorString) {
+
+  //if (document.body.contains(document.getElementById("InputWarning"))) {
+    //var errorElement = document.getElementById("InputWarning");
+    //var children = errorElement.childNodes;
+    //for (var i = 0; i < children.length; i++) {
+    //  errorElement.removeChild(children[i]);
+    //}
+    //errorElement.appendChild(document.createTextNode(errorString));
+    //document.getElementById("InputWarning").innerHTML = "test string";
+//    document.body.innerHTML += '<p id="InputWarning">' + errorString + '</p>';
+    //document.body.innerHTML = "";
+//  } else {
+    //var errorElement = document.createElement("p");
+    //errorElement.setAttribute("id", "InputWarning");
+    //errorElement.appendChild(document.createTextNode(errorString));
+//    document.body.innerHTML += '<p id="InputWarning" style="color:#ff0000">' + errorString + '</p>';
+//  }
+//}
+//The above function was meant to edit an error element if it exists, but as soon as it tries to edit an existing element it exits the function without returning for some reason
+
+function ensureCorrectInput(inputText) { //checks to make sure the input text is valid, and prevents submission if it is
   var legalCharacters = /^[A-Za-z ]+$/;
   var spaces = /^[ ]+$/;
 
   if (!(inputText.value.match(legalCharacters))) {
 
-    document.body.innerHTML += '<p style="color:#ff0000">Only alphabet letters and spaces are allowed.</p>';
+    document.body.innerHTML += '<p id="InputWarning" style="color:#ff0000">Only alphabet letters and spaces are allowed.</p>';
     return false;
+
   } else if (inputText.value.match(spaces)) {
 
-    document.body.innerHTML += '<p style="color:#ff0000">Please input a name.</p>';
+    document.body.innerHTML += '<p id="InputWarning" style="color:#ff0000">Please input a name.</p>';
     return false;
 
   }
@@ -16,7 +38,7 @@ function ensureCorrectInput(inputText) {
   return true;
 }
 
-function shuffle(array) {
+function shuffle(array) { //Shuffles an array
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   // While there remain elements to shuffle...
@@ -35,7 +57,7 @@ function shuffle(array) {
   return array;
 }
 
-function cleanQueryString(queryString) {
+function cleanQueryString(queryString) { //Turns the queryString into an array, removes symbols and keys, then returns the array.
   queryString = queryString.substring(1);
   const dataArray = queryString.split("&");
   const nameList = [];
@@ -49,11 +71,11 @@ function cleanQueryString(queryString) {
   return nameList;
 }
 
-function getRandomInt(max) {
+function getRandomInt(max) { //returns a random int from 0 to max
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function matchNames() {
+function matchNames() { //Returns an array where the elements are arrays containing each name along with a randomly selected name 
 
   const matchList = [];
   shuffle(nameList);

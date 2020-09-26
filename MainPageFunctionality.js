b5b0856b8b9c9426e0697f2b759cf7abf2b9ac9a
Thomas Heath <thomasheathtgh@gmail.com>
2020-09-26 20:51:41 +0100

    Scrapped my custom build input validation for a Web API, switched all innerHTML uses with proper DOM Manipulation.

--- MainPageFunctionality.js
+++ MainPageFunctionality.js
@@ -1,41 +1,13 @@
-//function displayInputError(errorString) {
+function displayInputError() {
 
-  //if (document.body.contains(document.getElementById("InputWarning"))) {
-    //var errorElement = document.getElementById("InputWarning");
-    //var children = errorElement.childNodes;
-    //for (var i = 0; i < children.length; i++) {
-    //  errorElement.removeChild(children[i]);
-    //}
-    //errorElement.appendChild(document.createTextNode(errorString));
-    //document.getElementById("InputWarning").innerHTML = "test string";
-//    document.body.innerHTML += '<p id="InputWarning">' + errorString + '</p>';
-    //document.body.innerHTML = "";
-//  } else {
-    //var errorElement = document.createElement("p");
-    //errorElement.setAttribute("id", "InputWarning");
-    //errorElement.appendChild(document.createTextNode(errorString));
-//    document.body.innerHTML += '<p id="InputWarning" style="color:#ff0000">' + errorString + '</p>';
-//  }
-//}
-//The above function was meant to edit an error element if it exists, but as soon as it tries to edit an existing element it exits the function without returning for some reason
+  var warningElement = document.getElementById("InputWarning");
+  var warningMessage = document.createTextNode("Only alphabet letters and spaces are allowed.");
+  var children = warningElement.childNodes;
 
-function ensureCorrectInput(inputText) { //checks to make sure the input text is valid, and prevents submission if it is
-  var legalCharacters = /^[A-Za-z ]+$/;
-  var spaces = /^[ ]+$/;
-
-  if (!(inputText.value.match(legalCharacters))) {
-
-    document.body.innerHTML += '<p id="InputWarning" style="color:#ff0000">Only alphabet letters and spaces are allowed.</p>';
-    return false;
-
-  } else if (inputText.value.match(spaces)) {
-
-    document.body.innerHTML += '<p id="InputWarning" style="color:#ff0000">Please input a name.</p>';
-    return false;
-
+  for (var i = 0; i < children.length; i++) {
+    warningElement.removeChild(children[i]);
   }
-
-  return true;
+  warningElement.appendChild(warningMessage);
 }
 
 function shuffle(array) { //Shuffles an array
@@ -120,27 +92,105 @@ function matchNames() { //Returns an array where the elements are arrays contain
   }
 }
 
+var errorDisplay = document.createElement("p");
+errorDisplay.setAttribute("id", "InputWarning");
+errorDisplay.style.color = "#ff0000";
+
 var queryString = window.location.search;
 if ( queryString == "" ) {
-  document.body.innerHTML += "<h2>Add a participant!</h2>";
-  document.body.innerHTML += '<form name="form1" accept-charset="ISO-8859-1"><input type="text" id="n1" name="n1"><input type="submit" value="Add" onclick="ensureCorrectInput(document.form1.n1)"></form>';
+
+  //Builds all the elements
+  var h2 = document.createElement("h2");
+  h2.appendChild(document.createTextNode("Add a participant!"));
+
+  var form1 = document.createElement("form");
+  form1.setAttribute("name", "form1");
+
+  var n1Input = document.createElement("input");
+  n1Input.setAttribute("type", "text");
+  n1Input.setAttribute("id", "n1");
+  n1Input.setAttribute("name", "n1");
+  n1Input.setAttribute("required", "");
+  n1Input.setAttribute("pattern", "^[A-Za-z ]+$");
+
+  var submitInput = document.createElement("input");
+  submitInput.setAttribute("type", "submit");
+  submitInput.setAttribute("value", "Add");
+  submitInput.setAttribute("onclick", "displayInputError()");
+
+  //puts the elements into the document
+  document.body.appendChild(h2);
+  form1.appendChild(n1Input);
+  form1.appendChild(submitInput);
+  document.body.appendChild(form1);
+  document.body.appendChild(errorDisplay);
+
 } else {
   const nameList = cleanQueryString(queryString);
   var nameCount = nameList.length;
-  var previousDataForm = "";
-
-  document.body.innerHTML += ("<h2>Participants: " + nameCount + "</h2>");
-
+  var hiddenInput1;
   var name;
   var nameIndex;
+  var listItem;
+
+  var h2 = document.createElement("h2");
+  headerText = "Participants: " + nameCount;
+  h2.appendChild(document.createTextNode(headerText));
+
+  var form1 = document.createElement("form");
+  form1.setAttribute("name", "form1");
+
+  var form2 = document.createElement("form");
+  form2.setAttribute("name", "form2");
+  form2.setAttribute("action", "MatchedSantas");
+
+  var list = document.createElement("ul");
+  list.style.listStyle = "none";
+
   for (var i = 0; i < nameCount; i++) {
     name = nameList[i];
     nameIndex = i + 1;
-    document.body.innerHTML += ("<p" + nameIndex + ">" + name + "</p" + nameIndex + "><br>");
-    previousDataForm += ('<input type="hidden" name="n' + nameIndex + '" value="'  + name + '">');
+
+    listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(name));
+
+    list.appendChild(listItem);
+
+    hiddenInput1 = document.createElement("input");
+    hiddenInput1.setAttribute("type", "hidden");
+    hiddenInput1.setAttribute("name", "n" + nameIndex);
+    hiddenInput1.setAttribute("value", name);
+
+    hiddenInput2 = hiddenInput1.cloneNode();
+
+    form1.appendChild(hiddenInput1);
+    form2.appendChild(hiddenInput2);
   }
 
-  document.body.innerHTML += ('<form name="form1" accept-charset="ISO-8859-1">' + previousDataForm + '<input type="text" id="n1" name="n' + (nameCount + 1) + '"><input type="submit" value="Add" onclick="ensureCorrectInput(document.form1.n' + (nameCount + 1) + ')"></form>');
+  var input = document.createElement("input");
+  input.setAttribute("type", "text");
+  input.setAttribute("id", "n" + (nameCount + 1));
+  input.setAttribute("name", "n" + (nameCount + 1));
+  input.setAttribute("required", "");
+  input.setAttribute("pattern", "^[A-Za-z ]+$");
 
-  document.body.innerHTML += ('<form name="form2" action="MatchedSantas">' + previousDataForm + '<input type="submit" value="Go!"></form>');
+  var submitInput1 = document.createElement("input");
+  submitInput1.setAttribute("type", "submit");
+  submitInput1.setAttribute("value", "Add");
+  submitInput1.setAttribute("onclick", "displayInputError()");
+
+  form1.appendChild(input);
+  form1.appendChild(submitInput1);
+
+  var submitInput2 = document.createElement("input");
+  submitInput2.setAttribute("type", "submit");
+  submitInput2.setAttribute("value", "Go!");
+
+  form2.appendChild(submitInput2);
+
+  document.body.appendChild(h2);
+  document.body.appendChild(list);
+  document.body.appendChild(form1);
+  document.body.appendChild(errorDisplay);
+  document.body.appendChild(form2);
 }

00116eabba6cce40c4d8c7ab7cb3dcdc6e7f3b8a
Thomas Heath <thomasheathtgh@gmail.com>
2021-08-11 14:09:36 +0100

    CSP Testing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = new express;
 const port = process.env.PORT || 9090;
 
-res.set("Content-Security-Policy", "script-src 'sha256-gTfbez15CZAVoQ83OeaSVSseTjL3bRCxT70vZrg3I9I='");
+res.append("Content-Security-Policy", "script-src 'sha256-gTfbez15CZAVoQ83OeaSVSseTjL3bRCxT70vZrg3I9I='");
 
 app.get("/", function(req, res){res.sendfile("./MainPage.html")});
 app.get("/MatchedSantas", function(req, res){res.sendfile("./SecondPage.html")});

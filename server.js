c32c2aec0a1e5c20cadaaf5a98b18b0a68d06cad
Thomas Heath <thomasheathtgh@gmail.com>
2021-08-11 14:00:36 +0100

    Added a CSP Policy to test it

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = new express;
 const port = process.env.PORT || 9090;
 
+res.set("Content-Security-Policy", "script-src 'sha256-TiQT/8XjWSgRBdzJ52KzmEE9xTRXi1IDz09PBCu8W7g='");
+
 app.get("/", function(req, res){res.sendfile("./MainPage.html")});
 app.get("/MatchedSantas", function(req, res){res.sendfile("./SecondPage.html")});
 app.get("/Result", function(req, res){res.sendfile("./ResultsPage.html")});

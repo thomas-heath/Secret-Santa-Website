2a39e853637bb5286a23a9732169d8b9aaed2fa0
Thomas Heath <thomasheathtgh@gmail.com>
2021-08-11 14:41:38 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const express = require("express");
 const app = new express;
 const port = process.env.PORT || 9090;
 
-<<<<<<< HEAD
-app.get("/", function(req, res){
-    res.append("Content-Security-Policy-Report-Only", "script-src 'sha256-TiQT/8XjWSgRBdzJ52KzmEE9xTRXi1IDz09PBCu8W7g='")
-    res.sendfile("./MainPage.html")});
-=======
 app.get("/", function(req, res){res.sendfile("./MainPage.html")});
->>>>>>> parent of c32c2ae (Added a CSP Policy to test it)
 app.get("/MatchedSantas", function(req, res){res.sendfile("./SecondPage.html")});
 app.get("/Result", function(req, res){res.sendfile("./ResultsPage.html")});
 app.get("/SharedFunctions.js", function(req, res){res.sendfile("./SharedFunctions.js")});

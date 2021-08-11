a33b5f5789ef75171ee2fc752a6ddb10327a4d7d
Thomas Heath <thomasheathtgh@gmail.com>
2021-08-11 14:12:54 +0100

    CSP testing

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = new express;
 const port = process.env.PORT || 9090;
 
-res.append("Content-Security-Policy", "script-src 'sha256-gTfbez15CZAVoQ83OeaSVSseTjL3bRCxT70vZrg3I9I='");
-
-app.get("/", function(req, res){res.sendfile("./MainPage.html")});
+app.get("/", function(req, res){
+    res.append("Content-Security-Policy-Report-Only", "script-src 'sha256-gTfbez15CZAVoQ83OeaSVSseTjL3bRCxT70vZrg3I9I='")
+    res.sendfile("./MainPage.html")});
 app.get("/MatchedSantas", function(req, res){res.sendfile("./SecondPage.html")});
 app.get("/Result", function(req, res){res.sendfile("./ResultsPage.html")});
 app.get("/SharedFunctions.js", function(req, res){res.sendfile("./SharedFunctions.js")});
